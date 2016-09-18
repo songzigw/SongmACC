bbf1821b53e3db740baee84a9f6cca5d6f151419
songzigw <songzigw@163.com>
2016-09-18 21:36:48 +0800

    account

--- src/main/java/songm/account/web/UserController.java
+++ src/main/java/songm/account/web/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/")
 public class UserController {
 
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    @RequestMapping(value = "login", method = RequestMethod.GET)
     public String toLogin() {
 
         return "/login";
